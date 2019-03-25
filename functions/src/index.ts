6cfa16007059e244207231d6b66cf02bc44466bc
snakagam <snakagam@yahoo-corp.jp>
2019-03-25 11:35:25 +0900

    fix

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -14,10 +14,66 @@
 //
 
 // TODO(DEVELOPER): Import the Cloud Functions for Firebase and the Firebase Admin modules here.
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+type Firestore = admin.firestore.Firestore;
+const app = admin.initializeApp();
 
 // TODO(DEVELOPER): Write the computeAverageReview Function here.
+export const computeAverageReview = functions.firestore
+  .document('reviews/{reviewId}').onWrite((change, context) => {
+    // get the data from the write event
+    const eventData = change.after.data();
+    // get the previous value, if it exists
+    const prev = change.before;
+    const rating = eventData.rating;
+    let previousValue
+    if (prev.exists) {
+        previousValue = prev.data();
+        const prevRating = previousValue.rating;
+        if (rating === prevRating) {
+            console.log("not a new rating.");
+            return null;
+        }
+    }
+    // get the restaurant ID
+    const restaurantID = eventData.restaurantID;
+    // get a reference to the root of the firestore DB
+    const db = app.firestore()
+    // if a previous value exists, then it needs to be replaced
+    // when computing an average. Otherwise, add the new rating
+    if (prev.exists) {
+        const difference = previousValue.rating - rating
+        return updateAverage(db, restaurantID, difference, true);
+    } else {
+        return updateAverage(db, restaurantID, rating, false);
+    }
+  });
 
 // TODO(DEVELOPER): Add updateAverage helper function here.
+
+async function updateAverage(db: Firestore, restaurantID: string, newRating: number, prev: boolean) {
+  const updateDB = db.collection('restaurants').doc(restaurantID);
+  const restaurantDoc = await updateDB.get();
+  if (!restaurantDoc.exists) {
+      console.log("Document does not exist!");
+      return null;
+  }
+  const oldRating = restaurantDoc.data().averageRating;
+  const oldNumReviews = restaurantDoc.data().reviewCount;
+  let newNumReviews = oldNumReviews+1;
+  let newAvgRating = ((oldRating*oldNumReviews)+newRating)/newNumReviews;
+  // no need to increase review numbers if not a new review
+  // subtract the different made by the review
+  if (prev) {
+    newNumReviews = oldNumReviews;
+    newAvgRating = ((oldRating*oldNumReviews)-newRating)/oldNumReviews;
+  }
+  await updateDB.update({averageRating: newAvgRating, reviewCount: newNumReviews});
+  console.log("average updated");
+  return null;
+}
 
 // TODO(DEVELOPER): Write the updateRest Function here.
 
